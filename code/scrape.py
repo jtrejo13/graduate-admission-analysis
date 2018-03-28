39f20ad3a843d3a1629b5ce7fc3a26fe90e52320
jtrejo13 <jtrejo13@utexas.edu>
2018-03-28 17:20:40 -0500

    Initial commit

---
+++ code/scrape.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup as soup
+
+url_generic = 'https://thegradcafe.com/survey/index.php?q=computer%2A&t=a&pp=250&o=d&p={0}'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) \
+ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+
+for i in range(1, 186):
+    url = url_generic.format(i)
+    r = requests.get(url, headers=headers)
+    html = r.text
+    page = soup(html, 'html.parser')
+    file_name = './raw_data/page_{page}.html'.format(page=i)
+    with open(file_name, 'w') as f:
+        f.write(r.text)
+    print('reading page {page}...'.format(page=i))
