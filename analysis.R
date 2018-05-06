d9c1ba72e6ba9cf75c316a4cca4626269e9e8363
jtrejo13 <jtrejo13@utexas.edu>
2018-05-06 14:31:01 -0500

    Add analysis code and final data set

---
+++ analysis.R
@@ -0,0 +1,178 @@
+
+# # # # # # # # # # # # # # # # # # # #
+# Project: Graduate Admission Analysis
+# # # # # # # # # # # # # # # # # # # #
+
+library(SDSRegressionR)
+
+# Import data
+adm_data <- read.csv("data/cs_data.csv", stringsAsFactors = FALSE)
+
+# Add new column with binary decision values (0/1)
+adm_data$decision_bin <- NA
+adm_data$decision_bin[adm_data$decision == "Rejected"] <- 0
+adm_data$decision_bin[adm_data$decision == "Accepted"] <- 1
+
+# Factorize Variables
+table(adm_data$decision)
+adm_data$decision <- factor(adm_data$decision, levels=c("Rejected","Accepted"))
+
+table(adm_data$status)
+adm_data$status <- factor(adm_data$status, levels=c("A", "U", "I"))
+
+table(adm_data$degree)
+adm_data$degree <- factor(adm_data$degree, levels=c("Masters", "PhD"))
+
+admission <- adm_data[1:500, ]
+names(admission)
+
+# Summary
+summary(admission)
+
+# Data distribution check
+hist(admission$ranking)
+hist(admission$year)
+hist(admission$gpafin)
+hist(admission$gretot)
+
+# Intital Model
+adm_mod_0 <- glm(decision ~ gretot + gpafin + status + degree + ranking + status*gpafin, data=admission, family="binomial")
+summary(adm_mod_0)
+
+# Assumption Check
+library(car)
+vif(adm_mod_0)
+cooksPlot(adm_mod_0, key.variable = "id", print.obs = TRUE, sort.obs = TRUE)
+threeOuts(adm_mod_0, key.variable = "id")
+
+# Remove outliers
+"%not in%" <- Negate("%in%")
+g_admission <- admission[admission$id %not in% c('384'),]
+
+# Re-run
+adm_mod <- glm(decision ~ gretot + gpafin + status + degree + ranking + status*gpafin, data=g_admission, family="binomial")
+summary(adm_mod)
+
+# Odds-ratios
+exp(adm_mod$coef)
+exp(confint.default(adm_mod))
+
+# Stats
+library(rms)
+adm_mod_2 <- lrm(decision ~ gretot + gpafin + status + degree + ranking + status*gpafin, g_admission)
+adm_mod_2
+
+# Pseudo R2
+library(DescTools)
+PseudoR2(adm_mod, which = "Nagelkerke")
+
+# Test the full categorical variable
+library(car)
+Anova(adm_mod, type="III")
+
+# Post-hoc testing
+library(lsmeans)
+lsmeans(adm_mod, "status", type="response")
+pairs(lsmeans(adm_mod, "status"), reverse=TRUE, adjust="none")
+
+summary(adm_mod)
+ref.grid(adm_mod)
+
+# # PSA
+# alpha_PSA <- -5.373861
+# beta_PSA  <- 0.032964
+# omega_PSA <- (-0.027747 * 66.145) + (-0.014582 * 16.264) + (1.025764 * 6.3669)
+# vmark_PSA <- (log(0.50 / (1 - 0.50)) - alpha_PSA - omega_PSA) / beta_PSA
+# vmark_PSA
+# 
+# # GLEASON
+# alpha_G <- -5.373861
+# beta_G <- 1.025764
+# omega_G <- (-0.027747 * 66.145) + (-0.014582 * 16.264) + (0.032964 * 15.377)
+# vmark_G <- (log(0.50 / (1 - 0.50)) - alpha_G - omega_G) / beta_G
+# vmark_G
+
+# # # # # # # #
+# Prediction  #
+# # # # # # # #
+
+# Bring in new data:
+admission_test <- adm_data[501:510, ]
+table(admission_test$decision)
+table(admission_test$status)
+hist(admission_test$gretot)
+
+odds <- exp(predict(adm_mod, admission_test))
+admission_test <- data.frame(admission_test, admission_model=odds/(odds+1))
+table(admission_test$id, admission_test$decision, admission_test$admission_model)
+
+# # # # #
+# Plots #
+# # # # #
+
+# Moderation
+lmDecomp(adm_mod, "statusU", "gpafin", mod.type=2, mod.values = c(1, 2, 3), print.sslopes = FALSE)
+
+
+admission_mns_1 <- summary(lsmeans(adm_mod, "gpafin",
+                                 at=list(gpafin = seq(2.45, 4, 0.1)), by="status", type="response"))
+
+simpleScatter(g_admission, gpafin, decision_bin, ptalpha = 0,
+              title="Quality of Life:Psychological Assessment and State Anxiety by Student Group") +
+  geom_line(data=admission_mns_1, aes(x=gpafin, y=prob, color=status)) +
+  geom_ribbon(data=admission_mns_1, aes(y=prob, ymin=asymp.LCL, ymax=asymp.UCL, group=status), alpha=0.1) 
+  # #Change to your group names and number of groups
+  # scale_colour_manual(name = "Status",
+                      # values =c("red", "blue", "green"),
+                      # labels = c("American, US Degree", "Not American, US Degree", "International"))
+
+# Look at ranges...
+summary(g_admission)
+
+# Predict Status
+mns <- summary(lsmeans(adm_mod, "status", type="response"))
+
+# Graph Status
+library(ggplot2)
+ggplot(mns, aes(x=status, y=prob)) +
+  geom_bar(stat="identity") +
+  geom_errorbar(aes(ymin=asymp.LCL, ymax=asymp.UCL), width=0.2) +
+  theme_bw()
+
+# Predict GPA
+library(lsmeans)
+admission_mns_gpa <- summary(lsmeans(adm_mod, "gpafin",
+                                 at=list(gpafin = seq(2.45, 4, 0.1)), type="response"))
+
+# Graph GPA
+g <- simpleScatter(g_admission, gpafin, decision_bin, title="Decision Graph",
+                   xlab="GPA", ylab="P(Admission)")
+g +
+  geom_line(data=admission_mns_gpa, aes(x=gpafin, y=prob), color="red") +
+  geom_line(data=admission_mns_gpa, aes(x=gpafin, y=asymp.LCL), linetype="dashed") +
+  geom_line(data=admission_mns_gpa, aes(x=gpafin, y=asymp.UCL), linetype="dashed")
+
+# Predict GRE
+admission_mns <- summary(lsmeans(adm_mod, "gretot",
+                                 at=list(gretot = seq(297, 340, 10)), type="response"))
+
+# Graph GRE
+g1 <- simpleScatter(g_admission, gretot, decision_bin, title="Decision Graph",
+                   xlab="GRE", ylab="P(Admission)")
+g1 +
+  geom_line(data=admission_mns, aes(x=gretot, y=prob), color="red") +
+  geom_line(data=admission_mns, aes(x=gretot, y=asymp.LCL), linetype="dashed") +
+  geom_line(data=admission_mns, aes(x=gretot, y=asymp.UCL), linetype="dashed")
+
+# Predict Ranking
+admission_mns_rank <- summary(lsmeans(adm_mod, "ranking",
+                                 at=list(ranking=seq(2.3, 5, 0.1)), type="response"))
+
+# Graph Ranking
+g2 <- simpleScatter(g_admission, ranking, decision_bin, title="Decision Plot",
+                   xlab="Ranking", ylab="P(Admission)")
+g2 +
+  geom_line(data=admission_mns_rank, aes(x=ranking, y=prob), color="red") +
+  geom_line(data=admission_mns_rank, aes(x=ranking, y=asymp.LCL), linetype="dashed") +
+  geom_line(data=admission_mns_rank, aes(x=ranking, y=asymp.UCL), linetype="dashed")
+
